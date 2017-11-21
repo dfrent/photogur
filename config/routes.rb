0719ad91e7ef2c1ad790129f3ed2ac67c588976f
cordell <cordell.perry@gmail.com>
2017-11-21 15:57:42 -0500

    controller actions/views to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ get 'pictures' => 'pictures#index'
 post 'pictures' => 'pictures#create'
 get 'pictures/new' => 'pictures#new'
 
+get 'pictures/:id/edit' => "pictures#edit"
+patch 'pictures/:id' => "pictures#update"
 
 get 'pictures/:id' => 'pictures#show'
 
