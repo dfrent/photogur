da273e61f55e7e20d0177039167efb74b0fd85d0
cordell <cordell.perry@gmail.com>
2017-11-21 14:32:55 -0500

    dynamic route for pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,12 @@
 class PicturesController < ApplicationController
 
-def index
-  @pictures = Picture.all
-end
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
 
 end
